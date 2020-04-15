74d81fb8a9355c79920c601bd75f792735695e86
Jaimegiraldo2 <>
2020-04-15 17:21:21 -0400

    explanation update
    update

--- events/models.py
+++ events/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class Event(models.Model):
 
+    """ simple model of an event app taking only 
+    name of the event, start date it include the time,
+    end date also you could put the time, color and the
+    details of the event"""
+
     name = models.CharField(max_length=140)
     start = models.CharField(max_length=20)
     end = models.CharField(max_length=20)
